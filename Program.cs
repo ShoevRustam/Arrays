39d5e5798de24adbcb7bf53519d2b5d27ba671eb
ShoevRustam <67133591+ShoevRustam@users.noreply.github.com>
2021-07-12 13:41:15 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Arr {
+    class Program {
+        static void Main (string[] args) {
+            int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 21, 5, 1 };
+            foreach(var item in number){
+                System.Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
