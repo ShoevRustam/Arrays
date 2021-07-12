7516389f6c8bd74bc88fb39847d3fba93a2f20ad
ShoevRustam <67133591+ShoevRustam@users.noreply.github.com>
2021-07-12 14:45:59 +0300

    Any Change

--- Program.cs
+++ Program.cs
@@ -3,11 +3,19 @@
 namespace Arr {
     class Program {
         static void Main (string[] args) {
-            int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 21, 5, 1 };
-            foreach (var item in number) {
-                System.Console.WriteLine (item);
+
+            /*          int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 21, 5, 1 };
+                        foreach (var item in number)
+                        {
+                            System.Console.WriteLine (item);
+                        }
+                        Console.ReadKey ();
+            */
+            for (int i = 0; i <= 10; i++) {
+                i++;
+                System.Console.WriteLine ("Hello Word " + i);
+                Console.ReadKey ();
             }
-            Console.ReadKey ();
         }
     }
 }
